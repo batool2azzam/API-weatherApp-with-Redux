f850075874c9da045aeebc573f798a7de7261bb0
batool2azzam <batoolazzam16@gmail.com>
2023-12-20 22:08:04 +0200

    f

--- src/i18n.js
+++ src/i18n.js
@@ -17,8 +17,7 @@ i18n
 	.use(LanguageDetector)
 	// pass the i18n instance to react-i18next.
 	.use(initReactI18next)
-	// init i18next
-	// for all options read: https://www.i18next.com/overview/configuration-options
+
 	.init({
 		fallbackLng: "en",
 		debug: true,
